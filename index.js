d30287eec96bdb8dc5a2e4364ec340d0c5839e22
malhisimrat <malhisimrat07@gmail.com>
2025-07-24 15:01:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.static(__dirname + '/server/public/'));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: false, parameterLimit: 500000000 }));
 app.get('/', (req, res) => {
-    res.send('<h1> anni dea mazak e</h1> ')
+    res.send('<h1> fer kehnde a butta gallan kd da</h1> ')
 })
 
 app.use('/api', apiRoutes)
